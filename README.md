8b95de7a6e6d33f5a0ee6d3b21bfdaca5d103a4d
concurrentshell <nilanjanmridha89@gmail.com>
2024-07-28 20:24:49 +0000

    Sever added

---
+++ README.md
@@ -0,0 +1,4 @@
+# Simple Rock Paper Scissors game
+## How to use? 
+> Clone the git repo ```cd Rock-Paper-Scissors && node server.js```
+## Make sure you jave node js installed. Else it won't work. 😅
---
+++ server.js
@@ -0,0 +1,52 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const port = 3000;
+
+const requestListener = (req, res) => {
+    console.log(`Request URL: ${req.url}`);
+    
+    let filePath = path.join(__dirname, req.url === '/' ? 'index.html' : req.url);
+    const extname = path.extname(filePath);
+    let contentType = 'text/html';
+
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.jpg':
+            contentType = 'image/jpeg';
+            break;
+        case '.gif':
+            contentType = 'image/gif';
+            break;
+    }
+
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            if (err.code === 'ENOENT') {
+                res.writeHead(404, { 'Content-Type': 'text/html' });
+                res.end('<h1>404 Not Found</h1>', 'utf8');
+            } else {
+                res.writeHead(500);
+                res.end('Server Error');
+            }
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf8');
+        }
+    });
+};
+
+const server = http.createServer(requestListener);
+
+server.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
