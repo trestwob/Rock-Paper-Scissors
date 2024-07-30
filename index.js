e41da7001ea307a0e51aece9f2dc6206c4d6adfc
concurrentshell <nilanjanmridha89@gmail.com>
2024-07-30 07:57:18 +0000

    Local storage is added.

--- index.js
+++ index.js
@@ -12,12 +12,27 @@ function game(input) {
 
 //    console.log(possibleOutcomes[input]);
 
-    const result = input === value ? 'Draw 😳' : possibleOutcomes[input] === value ? 'You win.🤫' : 'You lose. 😂';
+    const result = input === value ? 'Draw.😳' : possibleOutcomes[input] === value ? 'You win.🤫' : 'You lose.😂';
     // it's like , if input === value return true, then it's a draw, else if possible outcomes === value return true then win or else lose 
     // possible outcomes is a dictionary and accessing the values with the input value 
     // input value is directly passed from the onclick function 
 
-
     document.getElementById("p2").textContent = `System: ${value}`;
     document.getElementById("p3").textContent = result;
+
+    scores(result); 
+}
+
+const score =
+    JSON.parse(localStorage.getItem('score')) || {
+    wins: 0,
+    loses: 0,
+    draws: 0,
+};
+
+function scores(input) {
+    document.getElementById('scores').textContent = `Wins: ${score.wins}, Losses: ${score.loses}, Draws: ${score.draws}`;
+    input == "You win.🤫" ? score.wins += 1 : input == "You lose.😂" ? score.loses += 1 : score.draws += 1;
+    localStorage.setItem('score', JSON.stringify(score));
+
 }
